dedfd86a657e3f0b2f9d4ef6ad4041b76a832be3
Suman Regmi <limitlessplaying@gmail.com>
2025-01-16 11:51:22 +0545

    Sum of first n even numbers

--- 09_sum_of_n_even_numbers.c
+++ 09_sum_of_n_even_numbers.c
@@ -4,17 +4,18 @@
 #include <conio.h>
 void main()
 {
-    int i, sum = 0, even, n;
+    int i,count=1, sum = 0, even, n;
     printf("\nHow many numbers do you want to add?\n");
     scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    for (i = 1; count <= n; i++)
     {
         if (i % 2 == 0)
         {
             printf("\nThe even number is: %d", i);
             sum = sum + i;
+            count++;
         }
     }
-    printf("\nThe sum is: %d", sum);
+    printf("\nThe sum of first %d even numbers is: %d", n, sum);
     getch();
 }
