0292c6bc3777f26b66bb7f6b4c2616c8f14db7b2
Suman Regmi <limitlessplaying@gmail.com>
2025-01-16 11:42:48 +0545

    Nature of a number

--- 05_Nature_of_a_Number.c
+++ 05_Nature_of_a_Number.c
@@ -6,28 +6,33 @@ int main()
 {
     /* To find whether the number is even or odd*/
     int a;
-    printf("\n \nEnter any number \n");
+    printf("\n \nEnter any number\n");
     scanf("%d", &a);
-    if (a % 2 == 0)
+    if (a == 0)
     {
-        printf("%d is even number", a);
+        printf("\n%d is neither even nor odd.", a);
+        printf("\n%d is neither positive nor negative.", a);
     }
     else
     {
-        printf("%d is odd number", a);
-    }
+        if (a % 2 == 0)
+        {
+            printf("\n%d is even number.", a);
+        }
+        else
+        {
+            printf("\n%d is odd number.", a);
+        }
 
-    /* To find whether the number is positive or negative*/
-    int b;
-    printf("\n \nEnter any number \n");
-    scanf("%d", &b);
-    if (b > 0)
-    {
-        printf("%d is positive number", b);
-    }
-    else
-    {
-        printf("%d is negative number", b);
+        /* To find whether the number is positive or negative*/
+        if (a > 0)
+        {
+            printf("\n%d is positive number.", a);
+        }
+        else
+        {
+            printf("\n%d is negative number.", a);
+        }
     }
     getch();
 }
