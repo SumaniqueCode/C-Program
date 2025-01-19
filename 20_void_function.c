a73a5f14b7dd3b30120b2ae83d1374dcc83c198b
Suman Regmi <limitlessplaying@gmail.com>
2025-01-19 17:42:57 +0545

    function to calculate sum

---
+++ 20_void_function.c
@@ -0,0 +1,18 @@
+// Created by Suman Regmi
+
+// Function with no arguments and no return value
+#include <stdio.h>
+
+void sum()
+{
+    int a, b;
+    printf("\nEnter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d", a, b, a + b);
+}
+
+void main()
+{
+    printf("\nTo calculate sum of two numbers using function.");
+    sum();
+}
