0c8bbee4e7f4d70631e534013d5cbc571df345d0
Suman Regmi <limitlessplaying@gmail.com>
2025-01-21 17:16:10 +0545

    Calculating SI by passing args in function

---
+++ 21_function_with_args.c
@@ -0,0 +1,24 @@
+// Created by Suman Regmi
+
+// Function with arguments but no return value
+#include <stdio.h>
+
+void SI(float p, float t, float r) // p, t, r is are arguments here
+{
+    float result;
+    result = (p * t * r) / 100;
+    printf("Simple Interest is: %.2f\n", result);
+}
+
+void main()
+{
+    float p, t, r;
+    printf("\nTo calculate SI");
+    printf("\nEnter Principle: ");
+    scanf("%f", &p);
+    printf("Enter Time in years: ");
+    scanf("%f", &t);
+    printf("Enter rate in percent: ");
+    scanf("%f", &r);
+    SI(p, t, r); // p,t,r is passed as parameter here
+}
