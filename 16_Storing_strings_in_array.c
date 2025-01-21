2ff82513dff1460182007d8d78c7a45f92eb1bc2
Suman Regmi <limitlessplaying@gmail.com>
2025-01-21 17:15:11 +0545

    Storing data in multidimensional array

--- 16_Storing_strings_in_array.c
+++ 16_Storing_strings_in_array.c
@@ -4,13 +4,14 @@
 void main()
 {
     int n, i;
-    char name[20][30]; // First for array length and second is for string lenth
+    //Strings can be defined in array by defining char in array.
+    char name[20][30]; // First for array length and second is for string length
     printf("\nHow many names do you want to store?\n");
     scanf("%d", &n);
     for (i = 0; i < n; i++)
     {
         printf("\nEnter the name:\t");
-        scanf("%s", name[i]);
+        scanf("%s", name[i]); // %s is used for string, no & sign is required for array as string uses pointer for char
     }
     printf("\nThe stored names are\n");
     for (i = 0; i < n; i++)
