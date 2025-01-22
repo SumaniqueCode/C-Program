39dc2b1e4d12de44f320a27089fac2c4fa272d85
Suman Regmi <limitlessplaying@gmail.com>
2025-01-22 10:32:52 +0545

    Sorting names

--- 17_Name_Sorting.c
+++ 17_Name_Sorting.c
@@ -18,9 +18,11 @@ int main()
     {
         for (j = i+1; j < n; j++)
         {
-            if (strcmp(name[i], name[j]) > 0)
+            //strcmp compares the ASCII value of first char from both variable.
+            //It return 1 if value first variable is greater, -1 if smaller and 0 if equal.
+            if (strcmp(name[i], name[j]) > 0) //strcmp and strcpy is given by <string.h> header file
             {
-                strcpy(temp, name[i]);
+                strcpy(temp, name[i]); //strcpy copies the string values of name[i] in temp variable
                 strcpy(name[i], name[j]);
                 strcpy(name[j], temp);
             }
